ceae6c36d043427ae0f4935115d2c28cfbd88059
arunrajora <rajora.rajora2@outlook.com>
2016-10-12 12:18:40 +0530

    add chit-chat module,chit-chat developer module,authenticator module

---
+++ app/src/main/java/com/rajora/arun/chat/chit/chitchat/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rajora.arun.chat.chit.chitchat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
